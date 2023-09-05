e0931f5d2d0210d1f1b5712b2f5b48871f2169f5
Ren3546 <renatovaldez6879@gmail.com>
2023-09-04 20:30:09 -0400

    fixed issue with prompt not opening

--- Develop/script.js
+++ Develop/script.js
@@ -10,22 +10,14 @@ function writePassword() {
 
 }
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   let passLength = prompt("Enter how many characters you would like (between 8 and 125)");
-  if (passLength >=8, passLength <=125) {
+  if (passLength >=8 && passLength<=125) {
     upper();
   }
   else {
-    alert('Please input a valid number')
+    alert ('Please input a valid number')
   }
- }
- 
- var charNumber = passLength;
- 
-
-//to set input params, make it so if statements, only goes to the next prompt if the number is between 8 and 125
-
-
-
+}
