f53790459e8f3157a5edf10edbca1ba8aac9266c
Ren3546 <renatovaldez6879@gmail.com>
2023-09-10 19:27:31 -0400

    button to copy password to clipboard

--- Develop/script.js
+++ Develop/script.js
@@ -118,6 +118,14 @@
   return window.res;
  }
  
+function myFunction() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+  alert("Copied the text: " + copyText.value);
+}
+ 
  
 
 
